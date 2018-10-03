664d59a945c25aa4172f512b656e4d9fbfd05af7
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2018-10-03 14:22:15 +0800

    added nav graph added activity_main

---
+++ app/src/main/java/com/cogitator/justnavigation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.cogitator.justnavigation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Ankit Kumar on 03/10/2018
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
