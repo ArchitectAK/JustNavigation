c000d8fc25fb2507748b65887d1f741827c56bb8
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2018-10-03 18:07:13 +0800

    working on main activity

---
+++ app/src/main/java/com/cogitator/justnavigation/FragmentTwo.kt
@@ -0,0 +1,16 @@
+package com.cogitator.justnavigation
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * @author Ankit Kumar on 03/10/2018
+ */
+class FragmentTwo : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+}
